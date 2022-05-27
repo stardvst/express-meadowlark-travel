5d7211cb3334425656037f2ab7bfae93a7248eca
stardvst <lilia.manukyan@hotmail.com>
2022-05-27 20:28:14 +0400

    added unit tests for routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const fortune = require('./lib/fortune.js');
+const handlers = require('./lib/handlers.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,26 +9,15 @@ const port = process.env.PORT || 3000;
 app.engine('.hbs', expressHandlebars.engine({ extname: '.hbs', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() });
-});
+app.get('/about', handlers.about);
 
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res) => {
-  res.status(404);
-  res.render('404');
-});
+app.use(handlers.notFound);
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
-});
+app.use(handlers.serverError);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
