0141c22f6dee2556873b1ecf458576fe73c8a4d1
stardvst <lilia.manukyan@hotmail.com>
2022-06-05 18:26:59 +0400

    layouts and partials

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const handlers = require('./lib/handlers');
+const weatherMiddlware = require('./lib/middleware/weather');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.disable('x-powered-by');
-app.engine('.hbs', expressHandlebars.engine({ extname: '.hbs', defaultLayout: 'main' }));
+app.engine(
+  '.hbs',
+  expressHandlebars.engine({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    helpers: {
+      section(name, options) {
+        // eslint-disable-next-line no-underscore-dangle
+        if (!this._sections) this._sections = {};
+        // eslint-disable-next-line no-underscore-dangle
+        this._sections[name] = options.fn(this);
+        return null;
+      },
+    },
+  }),
+);
 app.set('view engine', '.hbs');
+
+app.use(express.static(`${__dirname}/public`));
+app.use(weatherMiddlware);
 
 app.get('/', handlers.home);
 
 app.get('/about', handlers.about);
+
+app.get('/section-test', handlers.sectionTest);
 
 app.get('/headers', (req, res) => {
   res.type('text/plain');
   const headers = Object.entries(req.headers).map(([key, value]) => `${key}: ${value}`);
   res.send(headers.join('\n'));
 });
-
-app.use(express.static(`${__dirname}/public`));
 
 app.use(handlers.notFound);
 
